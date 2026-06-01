850246dd856bb0b72406e97ab45ef27ca3626f49
MohammadHamdi11 <mohammadhamdisaid.mh@icloud.com>
2026-06-01 20:28:59 +0300

    fix netlify issue

--- members-portal/next.config.ts
+++ members-portal/next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from 'next'
 import withPWA from 'next-pwa'
 
 const nextConfig: NextConfig = {
-    output: 'export',
     images: {
-        unoptimized: true, // required for static export
-        // remotePatterns removed for static export
+        unoptimized: true,
     },
 }
 
